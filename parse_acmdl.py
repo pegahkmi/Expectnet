6759f1aa5876ba3179bf4829e6ab719137e6cf7a
kazjon <kazjon@me.com>
2016-12-27 16:13:19 -0500

    Fixed fast iterator path again.  Removed a whole bunch of old dead code (sentencereader etc)

--- parse_acmdl.py
+++ parse_acmdl.py
@@ -13,104 +13,10 @@ from scipy.stats import fisher_exact
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 logger = logging.getLogger("expectnet")
-inputfile = "ACMDL/PIQUE_Research papers_v1.0.csv"
-
-'''
-def calc_word_pmis(args):
-	return _calc_word_pmis(*args)
-
-def _calc_word_pmis(i_context, num_docs, corrs, counts):
-		n_context = counts[i_context]
-		p_context = n_context/num_docs
-		pmi_dict = {}
-		#pmi_row = np.zeros(num_words)
-		for i_feature,n_feature_and_context in corrs.iteritems():
-			if i_feature != i_context:
-				n_feature = counts[i_feature]
-				p_feature = n_feature/num_docs
-				n_feature_and_context = n_feature_and_context/num_docs
-				#pmi_row[i_feature] = np.log2(p_w1w2/(p_w1*p_w2))
-				pmi_dict[i_feature] = np.log2(n_feature_and_context/(p_context*p_feature))
-		return pmi_dict
-		#return pmi_row
-
-def _calc_word_odds_ratio(i_context,num_docs,corrs,counts, bayes=True):
-	p_context = counts[i_context]/num_docs
-	odds_dict = {}
-	for i_feature,n_feature_and_context in corrs.iteritems():
-		if i_feature != i_context:
-			if bayes:
-				p_feature = counts[i_feature]/num_docs
-				p_context_given_feature = (n_feature_and_context+1)/(counts[i_context]+2) #Constants are Laplace's
-				p_feature_given_context = (p_context_given_feature/p_context) * p_feature
-			else:
-				p_feature_given_context = counts[i_feature]/num_docs
-			odds_dict[i_feature] = np.log2(p_feature_given_context/p_context)
-	return odds_dict
-'''
-
-'''
-def calc_interpolated_log_conditional(i_context,num_docs,corrs,counts):
-	log_conditionals = {}
-	for i_feature,n_feature_and_context in corrs.iteritems():
-		if i_feature != i_context:
-			log_conditionals[i_feature] = calc_interpolated_log_conditional_pair(counts[i_feature], counts[i_context],n_feature_and_context,num_docs)
-	return log_conditionals
-
-def calc_interpolated_log_conditional_pair(n_feature, n_context, n_feature_and_context, n_docs):
-	marginal_prob = (n_feature+1)/(n_docs+2) # Constants are Laplace's correction (additive smoothing)
-	conditional_prob = (n_feature_and_context+1)/(n_context+2)
-	pvalue = _calc_significance_of_conditional(n_docs,n_feature,n_feature_and_context,n_context)
-	return np.log2(pvalue * conditional_prob + (1-pvalue) * marginal_prob)
-
-def _calc_significance_of_conditional(n_docs,n_docs_with_f,n_docs_with_f_and_c,n_docs_with_c):
-	return fisher_exact([[n_docs_with_f,n_docs-n_docs_with_f],[n_docs_with_f_and_c,n_docs_with_c-n_docs_with_f_and_c]])[1]
-'''
-
-class ACMDL_SentenceReader(object):
-	def __init__(self,path):
-		self.filepath = path
-		self.stop = stopwords.words("english")
-		self.tokeniser = RegexpTokenizer(r'\w+')
-		self.correlations = {"___total_words___": 0.0}
-		self.corrs_done = False
-
-	def __iter__(self):
-		with open(self.filepath,"rb") as i_f:
-			for row in csv.DictReader(i_f):
-				if not self.corrs_done:
-					docwords = [w for w in self.tokeniser.tokenize(row["Abstract"].lower()) if w not in self.stop]
-				for s in row["Abstract"].split("."):
-					r =  [w for w in self.tokeniser.tokenize(s.lower()) if w not in self.stop]
-					if not self.corrs_done:
-						for w1 in r:
-							if w1 not in self.correlations.keys():
-								self.correlations[w1] = {}
-								self.correlations[w1][w1] = 0.0
-							self.correlations[w1][w1] += 1
-							self.correlations["___total_words___"] += 1
-							for w2 in r: #Switching this between r and docwords switches the corrtab between per-sentence and per-abstract counts.
-								if w1 != w2:
-									if w2 not in self.correlations[w1].keys():
-										self.correlations[w1][w2] = 0.0
-									self.correlations[w1][w2] += 1
-					yield r
-		'''
-		if not self.corrs_done: #Now we have the entire vocab, go through the dictionary and say that words which aren't in there already occurred alongside each other 0 times.
-			for w1 in self.correlations.keys():
-				if w1 != "___total_words___":
-					for w2 in self.correlations.keys():
-						if w2 != "___total_words___":
-							if w2 not in self.correlations[w1].keys():
-								self.correlations[w1][w2] = 0.0
-		else:
-			self.corrs_done = True
-		'''
-		self.corrs_done = True
 
 class ACMDL_DocReader(object):
 	def __init__(self,path):
-		self.filepath = path+".csv"
+		self.filepath = path
 		self.stop = set(stopwords.words("english"))
 		self.tokeniser = RegexpTokenizer(r'\w+')
 		self.correlations = {"___total_words___": 0.0, "___total_docs___": 0.0}
@@ -118,7 +24,7 @@ class ACMDL_DocReader(object):
 		self.finalised = False
 
 	def __iter__(self):
-		with open(self.filepath,"rb") as i_f:
+		with open(self.filepath+".csv","rb") as i_f:
 			for row in csv.DictReader(i_f):
 				docwords = [w for w in self.tokeniser.tokenize(row["Abstract"].lower()) if w not in self.stop]
 				if not self.corrs_done:
@@ -142,9 +48,10 @@ class ACMDL_DocReader(object):
 
 	def process(self,suffix=".preprocessed"):
 		preprocessed_path = self.filepath+suffix
+		print preprocessed_path
 		if not os.path.exists(preprocessed_path):
 			logger.info(" ** Pre-processing started.")
-			with open(self.filepath+".preprocessed","wb") as pro_f:
+			with open(preprocessed_path,"wb") as pro_f:
 				writer = csv.writer(pro_f)
 				for doc in self:
 					writer.writerow(doc)
